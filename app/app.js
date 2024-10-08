c830511da5cca51269d5ba3971e5b110a4c0f1d4
amundead <150055552+amundead@users.noreply.github.com>
2024-10-09 00:22:51 +0800

    update

---
+++ app/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+const port = 8080;
+
+// Serve the static HTML file
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Endpoint to get host info
+app.get('/host-info', (req, res) => {
+  const hostname = os.hostname();
+  const ip = getIPAddress();
+  res.json({ hostname, ip });
+});
+
+// Helper function to get IP address
+function getIPAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const interfaceName in interfaces) {
+    for (const net of interfaces[interfaceName]) {
+      // Skip over non-IPv4 and internal (i.e., 127.0.0.1) addresses
+      if (net.family === 'IPv4' && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+  return 'IP not found';
+}
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
--- host-info.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Get the hostname and IP address
-HOSTNAME=$(hostname)
-IP=$(hostname -i)
-
-# Output the HTTP headers and the JSON response
-echo -e "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n"
-echo -e "{\"hostname\": \"${HOSTNAME}\", \"ip\": \"${IP}\"}"
