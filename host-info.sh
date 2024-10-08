6d349690a577ccf1e6998b412db7fb8390e4877f
amundead <150055552+amundead@users.noreply.github.com>
2024-10-08 23:58:43 +0800

    update

--- host-info.sh
+++ host-info.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-echo "{ \"hostname\": \"$(hostname)\", \"ip\": \"$(hostname -i)\" }"
+
+# Get the hostname and IP address
+HOSTNAME=$(hostname)
+IP=$(hostname -i)
+
+# Output the HTTP headers and the JSON response
+echo -e "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n"
+echo -e "{\"hostname\": \"${HOSTNAME}\", \"ip\": \"${IP}\"}"
