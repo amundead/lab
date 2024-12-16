6c362de2c5a503f8347ce0bbcc45f0320ff71a35
amundead <150055552+amundead@users.noreply.github.com>
2024-12-16 04:51:17 +0000

    s

--- minio-php/app/upload.php
+++ minio-php/app/upload.php
@@ -179,7 +179,7 @@ $phpVersion = phpversion();
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="upload.php">Upload</a>
@@ -200,7 +200,7 @@ $phpVersion = phpversion();
 <!-- Begin page content -->
 <main class="flex-shrink-0">
   <div class="container">
-    <h1 class="mt-5">Welcome. Hello World PHP - Nginx - Minio</h1>
+    <h1 class="mt-5">Welcome. Upload file to MINIO</h1>
     <p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code class="small">padding-top: 60px;</code> on the <code class="small">main &gt; .container</code>.</p>
     <p>Back to <a href="../examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
     </div>
