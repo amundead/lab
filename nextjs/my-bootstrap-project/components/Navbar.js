435e2e32e630fb115267433da0e7233815a73392
amundead <150055552+amundead@users.noreply.github.com>
2024-12-17 15:38:09 +0000

    s

--- nextjs/my-bootstrap-project/components/Navbar.js
+++
@@ -1,8 +0,0 @@
-export default function Navbar() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-      </nav>
-    );
-  }
-  
